9ec6b64de083264ad94e370a482926219e2eebc8
Tiago Loureiro <tiago.loureiro@gmail.com>
2014-12-26 17:13:15 +0000

    Added apns payload v2

--- notifier-apns/src/Network/Apns/Protocol.hs
+++ notifier-apns/src/Network/Apns/Protocol.hs
@@ -9,6 +9,7 @@ import Data.Word
 import Network.Apns.Types
 import Prelude hiding (length)
 
+import qualified Data.ByteString        as B
 import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString.Lazy   as BL
 
@@ -47,3 +48,39 @@ buildPDUv1 token payload expiry nId = runPut $ do
     putByteString token
     putWord16be (convert $ length payload)
     putByteString payload
+
+-- Details of the new PDU format can be checked at http://goo.gl/wtHDnm
+buildPDUv2 :: ByteString -> BL.ByteString -> Word32 -> Word32 -> BL.ByteString
+buildPDUv2 token payload expiry nId = runPut $ do
+    -- Command
+    putWord8 2
+    -- Frame length
+    putWord32be (fromIntegral frameLength)
+    ---- Frame data
+    -- Device token
+    putWord8 1
+    putWord16be (fromIntegral $ B.length token)
+    putByteString token
+    -- Payload
+    putWord8 2
+    putWord16be (fromIntegral $ BL.length payload)
+    putLazyByteString payload
+    -- Notification ID
+    putWord8 3
+    putWord16be 4
+    putWord32be nId
+    -- Expiration
+    putWord8 4
+    putWord16be 4
+    putWord32be expiry
+    -- Priority
+    putWord8 5
+    putWord16be 1
+    putWord8 10
+  where
+    frameLength = item1Len + item2Len + item3Len + item4Len + item5Len
+    item1Len = 1 + 2 + (B.length token)
+    item2Len = 1 + 2 + (fromIntegral $ BL.length payload)
+    item3Len = 1 + 2 + 4
+    item4Len = 1 + 2 + 4
+    item5Len = 1 + 2 + 1
